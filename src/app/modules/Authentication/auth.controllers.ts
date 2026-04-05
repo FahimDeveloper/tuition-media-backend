f04b377f64e5f138622ed3cd42f8ebcc03856a5b
FahimDeveloper <developerfahim246@gmail.com>
2026-04-05 20:34:04 +0600

    fix admin and user refresh token cookie setup

--- src/app/modules/Authentication/auth.controllers.ts
+++ src/app/modules/Authentication/auth.controllers.ts
@@ -57,15 +57,35 @@ const loginAdmin = catchAsync(async (req, res) => {
 });
 
 const refreshTeacherToken = catchAsync(async (req, res) => {
-  const { refresh_token } = req.body;
-  const result = await AuthServices.refreshTeacherTokenFromDB(refresh_token);
-  sendResponse(res, status.OK, 'Token refreshed successfully!', result);
+  const { refreshToken, user, accessToken } = await AuthServices.refreshTeacherTokenFromDB(
+    req.body,
+  );
+  res.cookie('refreshToken', refreshToken, {
+    // domain: '.prostrikers.com',
+    secure: true,
+    httpOnly: true,
+    sameSite: 'lax',
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+  });
+  sendResponse(res, status.OK, 'Token refreshed successfully!', {
+    user,
+    accessToken,
+  });
 });
 
 const refreshAdminToken = catchAsync(async (req, res) => {
-  const { refresh_token } = req.body;
-  const result = await AuthServices.refreshAdminTokenFromDB(refresh_token);
-  sendResponse(res, status.OK, 'Token refreshed successfully!', result);
+  const { refreshToken, user, accessToken } = await AuthServices.refreshAdminTokenFromDB(req.body);
+  res.cookie('refreshToken', refreshToken, {
+    // domain: '.prostrikers.com',
+    secure: true,
+    httpOnly: true,
+    sameSite: 'lax',
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+  });
+  sendResponse(res, status.OK, 'Token refreshed successfully!', {
+    user,
+    accessToken,
+  });
 });
 
 export const AuthControllers = {
